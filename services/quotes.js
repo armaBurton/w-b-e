a848b109b0ea7bb67202d540b9c9fc9329b42e24
armaBurton <armanarma@armaburton.com>
2022-11-29 12:53:52 -0800

    finalizing tutorial

--- services/quotes.js
+++ services/quotes.js
@@ -17,20 +17,20 @@ const getMultiple = async (page = 1) => {
   };
 };
 
-const validateCreate(quote) => {
+const validateCreate = (quote) => {
   let messages = [];
 
-  console.log('quote');
+  console.log("quote");
 
-  !quote && messages.push('No object is provided');
+  !quote && messages.push("No object is provided");
 
-  !quote.quote && messages.push('Quote is empty');
+  !quote.quote && messages.push("Quote is empty");
 
-  !quote.author && messages.push('Author is empty');
-  
-  quote.quote && quote.quote.length > 255 && messages.push('Author is empty');
-  
-  quote.author && quote.author.length > 255 && messages.push('Author is empty');
+  !quote.author && messages.push("Author is empty");
+
+  quote.quote && quote.quote.length > 255 && messages.push("Author is empty");
+
+  quote.author && quote.author.length > 255 && messages.push("Author is empty");
 
   if (messages.length) {
     let error = new Error(messages.join());
@@ -38,22 +38,23 @@ const validateCreate(quote) => {
 
     throw error;
   }
-}
+};
 
-const create = async (quote){
+const create = async (quote) => {
   validateCreate(quote);
 
   const result = await db.query(
-    'INSERT INTO quote(quote, author) VALUES ($1, $2) RETURNING *',
+    "INSERT INTO quote(quote, author) VALUES ($1, $2) RETURNING *",
     [quote.quote, quote.author]
   );
-  let message = 'Error in creating quote';
+  let message = "Error in creating quote";
 
-  result.length && (message = 'Quote Created Successfully');
+  result.length && (message = "Quote Created Successfully");
 
-  return {message}
-}
+  return { message };
+};
 
 module.exports = {
   getMultiple,
+  create,
 };
