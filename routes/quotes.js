1e320b4e74e0d33b700592a83e297df66703310f
armaBurton <armanarma@armaburton.com>
2022-11-28 17:59:44 -0800

    get route setup

--- routes/quotes.js
+++ routes/quotes.js
@@ -1,19 +1,27 @@
 const express = require("express");
 const router = express.Router();
+const quotes = require("../services/quotes");
 
 /* GET quotes listing */
-router.get("/", (req, res, next) => {
-  res.json({
-    data: [
-      {
-        quote: "First, solve the problem. Then, write the code.",
-        author: "John Johnson",
-      },
-    ],
-    meta: {
-      page: 1,
-    },
-  });
+router.get("/", async (req, res, next) => {
+  try {
+    res.json(await quotes.getMultiple(req.query.page));
+  } catch (error) {
+    console.error(`Error while getting quotes: ${error.message}`);
+    next(error);
+  }
+
+  // res.json({
+  //   data: [
+  //     {
+  //       quote: "First, solve the problem. Then, write the code.",
+  //       author: "John Johnson",
+  //     },
+  //   ],
+  //   meta: {
+  //     page: 1,
+  //   },
+  // });
 });
 
 module.exports = router;
--- services/quotes.js
+++ services/quotes.js
@@ -4,5 +4,19 @@ const config = require("../config");
 
 const getMultiple = async (page = 1) => {
   const offset = getOffset(page, config.listPerPage);
-  const rows = await db.query();
+  const rows = await db.query(
+    "SELECT id, quote, author FROM quote OFFSET $1 LIMIT $2",
+    [offset, config.listPerPage]
+  );
+  const data = emptyOrRows(rows);
+  const meta = { page };
+
+  return {
+    data,
+    meta,
+  };
+};
+
+module.exports = {
+  getMultiple,
 };
